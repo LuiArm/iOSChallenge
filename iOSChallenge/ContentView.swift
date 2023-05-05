ae99c1975f539fa5d262ca7d62757255ef1a9457
LuiArm <Luisarmendariz24@icloud.com>
2023-05-04 19:35:25 -0600

    Initial Commit

---
+++ iOSChallenge/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iOSChallenge
+//
+//  Created by luis armendariz on 5/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            List{
+                Text("Hello")
+            }
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSChallenge/iOSChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSChallengeApp.swift
+//  iOSChallenge
+//
+//  Created by luis armendariz on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
